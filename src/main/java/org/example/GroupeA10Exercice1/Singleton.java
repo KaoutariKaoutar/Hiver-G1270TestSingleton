547acb0669f16ae077414b63d34abce14b2224ea
KaoutariKaoutar <6282390@bdeb.qc.ca>
2024-06-20 15:44:09 -0400

    TestSingleton

---
+++ src/main/java/org/example/GroupeA10Exercice1/Singleton.java
@@ -0,0 +1,28 @@
+package org.example.GroupeA10Exercice1;
+
+public class Singleton {
+    private int nbrExecution =0;
+    // - instance
+    private static Singleton instance = null;
+
+    // - Singelton()
+    private Singleton()
+    {
+        System.out.println("constructeur singleton");
+        nbrExecution++;
+
+    }
+
+
+
+    // + getInstance()
+    public static Singleton getInstance()
+    {
+        if(instance == null)
+        {
+            instance =  new Singleton();
+        }
+        System.out.println("getInstance() ");
+        return instance;
+    }
+}
